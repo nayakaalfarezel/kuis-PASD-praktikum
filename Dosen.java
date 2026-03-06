8532047a2a7aafbf33b1bb1bb331b6a56a97c818
nayakaalfarezel <nayakaalfarezel422@gmail.com>
2026-03-06 09:15:03 +0700

    Dosen

---
+++ Dosen.java
@@ -0,0 +1,30 @@
+public class Dosen {
+    String nidn;
+    String nama;
+    int tahunMasuk;
+    String jenjangPendidikan;
+    String prodi;
+    
+    public Dosen(String nidn, String nama, int tahunMasuk, String jenjangPendidikan, String prodi) {
+        this.nidn = nidn;
+        this.nama = nama;
+        this.tahunMasuk = tahunMasuk;
+        this.jenjangPendidikan = jenjangPendidikan;
+        this.prodi = prodi;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("NIDN: " + nidn);
+        System.out.println("Nama: " + nama);
+        System.out.println("Tahun Masuk: " + tahunMasuk);
+        System.out.println("Jenjang Pendidikan: " + jenjangPendidikan);
+        System.out.println("Prodi: " + prodi);
+        System.out.println("-------------------------");
+    }
+    public void ubahProdi(String prodiBaru) {
+        this.prodi = prodiBaru;
+    }
+    public void ubahPendidikan(String pendidikanBaru) {
+        this.jenjangPendidikan = pendidikanBaru;
+    }
+}
